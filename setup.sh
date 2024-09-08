28f7feaf071fb83405907cf6ee3c89b4b83e5934
Jakub Wieloch <jakubvvieloch@gmail.com>
2024-09-08 15:46:19 +0200

    Created setup script
    Created Setup Script + added .vitetris config

--- setup.sh
+++ setup.sh
@@ -1,3 +1,5 @@
+#!/bin/bash
+
 #-----------------------------------------------------------------------------------------------------
 #       ██╗ █████╗ ██╗  ██╗██╗   ██╗██████╗     ██╗    ██╗██╗███████╗██╗      ██████╗  ██████╗██╗  ██╗
 #       ██║██╔══██╗██║ ██╔╝██║   ██║██╔══██╗    ██║    ██║██║██╔════╝██║     ██╔═══██╗██╔════╝██║  ██║
@@ -6,40 +8,7 @@
 #  ╚█████╔╝██║  ██║██║  ██╗╚██████╔╝██████╔╝    ╚███╔███╔╝██║███████╗███████╗╚██████╔╝╚██████╗██║  ██║
 #   ╚════╝ ╚═╝  ╚═╝╚═╝  ╚═╝ ╚═════╝ ╚═════╝      ╚══╝╚══╝ ╚═╝╚══════╝╚══════╝ ╚═════╝  ╚═════╝╚═╝  ╚═╝
 #-----------------------------------------------------------------------------------------------------
-# 						.zshrc
+# 					Dotfiles Install Script
 #-----------------------------------------------------------------------------------------------------
 
-# History + Vim Keybindings
-HISTFILE=~/.zsh_history
-HISTSIZE=99999
-SAVEHIST=99999
-bindkey -v
 
-# Basic auto/tab complete
-autoload -U compinit
-zstyle ':completion:*' menu select
-zmodload zsh/complist
-compinit
-_comp_options+=(globdots)	# Include hidden files
-
-# PS1 / Terminal promt
-PS1=$'\033[36m%n\033[m@\033[32m%m:\033[33;1m%~\033[m\$ '
-
-# Alias
-alias ls='ls --color=auto'
-alias ll='ls -lhA'
-alias la='ls -A'
-alias cp="cp -i"
-alias bat='batcat'
-
-# Editors / Export
-export VISUAL=vim
-export EDITOR="$VISUAL"
-export MANPAGER="nvim +Man!"
-
-# Plugins
-source ~/.config/.zsh/plugins/zsh-autosuggestions.zsh
-#source ~/.config/.zsh/plugins/zsh-syntax-highlighting.plugin.zsh
-
-# Zoxide initialation
-eval "$(zoxide init zsh)"
