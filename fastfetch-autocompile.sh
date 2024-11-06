3a055cb6bfdd238caa81b1221c871295849a2af2
Jakub Wieloch <jakubvvieloch@gmail.com>
2024-11-06 16:58:38 +0100

    Make fastfetch-autocompile.sh go fast ;-)

--- fastfetch-autocompile.sh
+++ fastfetch-autocompile.sh
@@ -1,6 +1,6 @@
-#!/bin/bash
+#!/bin/sh
 
-echo -ne "
+echo "
 -----------------------------------------------------------------------------------------------------
        ██╗ █████╗ ██╗  ██╗██╗   ██╗██████╗     ██╗    ██╗██╗███████╗██╗      ██████╗  ██████╗██╗  ██╗
        ██║██╔══██╗██║ ██╔╝██║   ██║██╔══██╗    ██║    ██║██║██╔════╝██║     ██╔═══██╗██╔════╝██║  ██║
@@ -9,24 +9,36 @@ echo -ne "
   ╚█████╔╝██║  ██║██║  ██╗╚██████╔╝██████╔╝    ╚███╔███╔╝██║███████╗███████╗╚██████╔╝╚██████╗██║  ██║
    ╚════╝ ╚═╝  ╚═╝╚═╝  ╚═╝ ╚═════╝ ╚═════╝      ╚══╝╚══╝ ╚═╝╚══════╝╚══════╝ ╚═════╝  ╚═════╝╚═╝  ╚═╝
 -----------------------------------------------------------------------------------------------------
- 				    Automated Fastfetch Compile script
+     			         Automated Fastfetch Compile script
 -----------------------------------------------------------------------------------------------------
 "
 
-# Download the newest releases Source code in .tar.gz form for the time being.
+# Download the newest releases source code in .tar.gz form for the time being.
 
 # Decompressing, Extracting & Changing Directories.
 gunzip fastfetch-*.tar.gz
 tar -xf fastfetch-*.tar
 cd fastfetch-*/
 
+# Download Build-dependancies
+
+install_dependencies() {
+    if which apt-get &> /dev/null; then
+        sudo apt-get update
+        sudo apt-get install -y cmake 
+    else
+        echo "Package manager not supported. Please install required programs & dependencies manually."
+    fi
+}
+
+install_dependencies
+
 # Compiling
 sudo cmake .
-sudo cmake --build . --target fastfetch --target flashfetch
+sudo cmake --build . --target fastfetch -j$(nproc)
 
 # Replacing old fastfetch binary & man-page
 sudo mv fastfetch /usr/bin
-sudo mv flastfetch /usr/bin
 sudo mv fastfetch.1 /usr/share/man/man1
 
 echo "Finished :-)"
